74263c51fb1143a2f26220b871bc78e96424102b
LeticiaG <leticiagenao403@gmail.com>
2024-06-01 01:57:01 -0400

    updating files

--- Part1_Local_and_Heroku_Deployment/app.py
+++ Part1_Local_and_Heroku_Deployment/app.py
@@ -72,5 +72,5 @@ def predict():
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  #  default to 5000
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000
     app.run(host='0.0.0.0', port=port, debug=True)

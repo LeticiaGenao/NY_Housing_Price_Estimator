08b568eca686cb3eca7ccdf516804e4d42eaf888
LeticiaG <leticiagenao403@gmail.com>
2024-06-01 01:30:55 -0400

    Procfile update

--- Part1_Local_and_Heroku_Deployment/gunicorn_config.py
+++
@@ -1,6 +0,0 @@
-# Gunicorn configuration file
-import os
-
-bind = "0.0.0.0:" + str(os.environ.get('PORT', 5000)) # Heroku automatically sets the PORT environment variable to 5000 if it isn't set, or to the host 0.0.0.0 on the port that the Gunicorn server is bound to. 
-workers = 3 #This determines how many worker processes are needed to handle requests. 
-
